6cc1f534358464f81261cdb370de76d30eac4f7f
TurtleY0da <timothy.veugelers@gmail.com>
2023-02-08 21:31:54 -0700

    Working on grid systems

--- modules/grid.js
+++ modules/grid.js
@@ -5,31 +5,49 @@ class gridSquare {
   }
 }
 
+function fillerFunction(e, index) {
+  return new this.fillerObject(
+    index % this.dimensions.w,
+    Math.floor(index / this.dimensions.w)
+  );
+}
+
 class grid {
   #flattenedArray = new Array();
   #sets = new Object();
+  fillerObject;
   dimensions = { w: 0, h: 0 };
-  constructor(width = 1, height = 1) {
+
+  constructor(width = 1, height = 1, fillerObject = gridSquare) {
     // Error catching
     if (!Number.isInteger(width))
       throw new TypeError("width is not an Integer");
     if (!Number.isInteger(height))
       throw new TypeError("height is not an Integer");
+
     if (width < 1) throw new RangeError("width is less than 1");
     if (height < 1) throw new RangeError("height is less than 1");
+
+    if (
+      !(
+        fillerObject.prototype instanceof gridSquare ||
+        fillerObject === gridSquare
+      )
+    )
+      throw new TypeError("fillerObject does not extend gridSquare");
+    //Set fillerObject
+    this.fillerObject = fillerObject;
+
     // Setting grid sizes
     this.dimensions = { w: width, h: height };
 
+    // Create 2 dimensional representation
     this.#flattenedArray = Array.from(
       { length: width * height },
-      (e, index) => {
-        return new gridSquare(
-          index % this.dimensions.w,
-          Math.floor(index / this.dimensions.w)
-        );
-      }
+      fillerFunction,
+      this
     );
   }
 }
 
-export { grid };
+export { grid, gridSquare };
--- project/game/Maze/main.js
+++ project/game/Maze/main.js
@@ -1,5 +1,15 @@
-import { grid } from "../../../modules/grid.js";
+import { grid, gridSquare } from "../../../modules/grid.js";
 
 let test = new grid(4, 5);
 
 console.log(test);
+
+class testClass extends gridSquare {
+  constructor(x, y) {
+    super(x, y);
+  }
+}
+
+let test2 = new grid(4, 5, testClass);
+
+console.log(test2);
