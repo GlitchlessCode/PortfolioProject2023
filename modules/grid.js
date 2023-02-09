6461b18a7018dfba56f4b1154a3e5439f6e54f42
TurtleY0da <90482934+TurtleY0da@users.noreply.github.com>
2023-02-09 11:23:04 -0700

    Added to grid systems

--- modules/grid.js
+++ modules/grid.js
@@ -48,6 +48,36 @@ class grid {
       this
     );
   }
+
+  getPos(x, y) {
+    if (!Number.isInteger(x)) throw new TypeError("x is not an Integer");
+    if (!Number.isInteger(y)) throw new TypeError("y is not an Integer");
+    if (x > this.dimensions.w - 1 || x < 0)
+      throw new RangeError("x is out of range");
+    if (y > this.dimensions.h - 1 || y < 0)
+      throw new RangeError("y is out of range");
+    return this.#flattenedArray[x + y * this.dimensions.w];
+  }
+
+  setPos(x, y, key, value) {
+    if (!Number.isInteger(x)) throw new TypeError("x is not an Integer");
+    if (!Number.isInteger(y)) throw new TypeError("y is not an Integer");
+    if (x > this.dimensions.w - 1 || x < 0)
+      throw new RangeError("x is out of range");
+    if (y > this.dimensions.h - 1 || y < 0)
+      throw new RangeError("y is out of range");
+    this.#flattenedArray[x + y * this.dimensions.w][key] = value;
+  }
+
+  createSet(name) {
+    if (this.#sets[name]) return;
+    this.#sets[name] = new Set();
+  }
+
+  removeSet(name) {
+    if (!(this.#sets[name] instanceof Set)) return;
+    delete this.#sets[name];
+  }
 }
 
 export { grid, gridSquare };
--- project/game/Maze/main.js
+++ project/game/Maze/main.js
@@ -1,15 +1,6 @@
-import { grid, gridSquare } from "../../../modules/grid.js";
+import { grid } from "../../../modules/grid.js";
 
 let test = new grid(4, 5);
 
-console.log(test);
-
-class testClass extends gridSquare {
-  constructor(x, y) {
-    super(x, y);
-  }
-}
-
-let test2 = new grid(4, 5, testClass);
-
-console.log(test2);
+// Temporary: Just for testing systems from console
+window.test = test;
