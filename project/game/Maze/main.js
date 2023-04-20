ca54d4ce44627fdcd60d87730d63cf954b28a9f7
TurtleY0da <90482934+TurtleY0da@users.noreply.github.com>
2023-04-20 11:23:28 -0600

    Working on Load dialog

--- project/game/Maze/main.js
+++ project/game/Maze/main.js
@@ -209,3 +209,39 @@ let test = new maze(24, 18, mazeSquare);
 
 window.test = test;
 window.sendFrame = sendFrame;
+
+// let dropZone = document.getElementById("drop_zone");
+
+// dropZone.addEventListener("drop", dropHandler);
+// dropZone.addEventListener("dragover", dragOverHandler);
+
+// function dropHandler(ev) {
+//   console.log("File(s) dropped");
+
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+
+//   if (ev.dataTransfer.items) {
+//     // Use DataTransferItemList interface to access the file(s)
+//     [...ev.dataTransfer.items].forEach(async (item, i) => {
+//       // If dropped items aren't files, reject them
+//       if (item.kind === "file") {
+//         const file = item.getAsFile();
+//         console.log(await file.text());
+//         console.log(`… file[${i}].name = ${file.name}`);
+//       }
+//     });
+//   } else {
+//     // Use DataTransfer interface to access the file(s)
+//     [...ev.dataTransfer.files].forEach((file, i) => {
+//       console.log(`… file[${i}].name = ${file.name}`);
+//     });
+//   }
+// }
+
+// function dragOverHandler(ev) {
+//   console.log("File(s) in drop zone");
+
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+// }
